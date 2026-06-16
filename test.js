3165557ce4ee5e68cea1974bfd019ca8af080485
Vikas Pandey <vikaspandey131118@gmail.com>
2026-06-16 09:18:26 +0000

    test: flash-lite for rate limits

--- test.js
+++ test.js
@@ -12,3 +12,4 @@ console.log('testing risk score feature')
 function fetchUser(id) { return db.query('SELECT * FROM users WHERE id = $1', [id]); }// quota reset test
 // retry after 503
 // permission fix v2
+// switch back to flash-lite
