9f1bda9f2c1ffdddaa5c19430648fff030fe1ee7
Vikas Pandey <vikaspandey131118@gmail.com>
2026-06-16 09:15:47 +0000

    test: personality permission fix

--- test.js
+++ test.js
@@ -11,3 +11,4 @@ console.log('testing risk score feature')
 // permission fix test
 function fetchUser(id) { return db.query('SELECT * FROM users WHERE id = $1', [id]); }// quota reset test
 // retry after 503
+// permission fix v2
